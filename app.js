320140cd8a9fd711edd3d5d5fb97914baf6a8ed4
Ariel <ariel.fuentesgarcia@gmail.com>
2022-05-11 20:59:04 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan'); 
+
+// Controllers
+const { globalErrorHandler } = require('./controllers/errors.controller');
+
+// Routers
+const { usersRouter } = require('./routes/users.routes');
+const { postsRouter } = require('./routes/posts.routes');
+const { commentsRouter } = require('./routes/comments.routes');
+
+// Init express app
+const app = express();
+
+// Enable CORS
+app.use(cors());
+// Add security headers
+app.use(helmet());
+// Compress responses
+app.use(compression());
+// log incomming requests
+// app.use(morgan('dev'));
+
+// Enable incoming JSON data
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'deveploment') app.use(morgan('dev'));
+else app.use(morgan('combined'));
+
+// Limit IP requests
+const limiter = rateLimit({
+  max: 10000,
+  windowMs: 1 * 60 * 60 * 1000, // 1 hr
+  message: 'Too many requests from this IP',
+});
+
+app.use(limiter);
+
+// Endpoints
+app.use('/api/v1/users', usersRouter);
+app.use('/api/v1/posts', postsRouter);
+app.use('/api/v1/comments', commentsRouter);
+
+// Global error handler
+app.use('*', globalErrorHandler);
+
+module.exports = { app };
